1991b652a97eda3a4d5d4e1f98f127bbd4b86731
Izaias <izaias.ignacio@outlook.com>
2020-10-23 16:15:23 -0300

    traducoes

---
+++ app/Models/Traducao.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+class Traducao {
+
+	public static function traducoes() {
+        return [
+            '직캠' => 'Fancam',
+            // RV
+            '레드벨벳' => 'Red Velvet',
+			'슬기' => 'Seulgi',
+			'아이린' => 'Irene',
+			'조이' => 'Joy',
+			'웬디' => 'Wendy',
+			'예리' => 'Yeri',
+			// SNSD
+            '태연' => 'Taeyeon',
+            // TW
+            '나연' => 'Naeyon',
+            '미나' => 'Mina',
+            '다현' => 'Dahyun',
+            '사나' => 'Sana',
+            '모모' => 'Momo',
+            '지효' => 'Jihyo',
+            '쯔위' => 'Tzuyu',
+            // IDLE
+			'우기' => 'Yuqi',
+			'수진' => 'Soojin',
+			'소연' => 'Soyeon',
+			'미연' => 'Miyeon',
+			'민니' => 'Minnie',
+            '슈화' => 'Shuhua',
+            // AOA
+            '유나' => 'Yuna',
+            '초아' => 'Choa',
+            '민아' => 'Mina',
+            '찬미' => 'Chanmi',
+            '설현' => 'Seolhyun',
+            // NM
+            '경리' => 'Kyungri',
+            '소진' => 'Sojin'
+		];
+	}
+    
+}
--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traducao;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
@@ -213,23 +214,7 @@ class Video extends Model {
 	}
 
 	private function traduzirTitulo($titulo) {
-		$traducoes = [
-			'레드벨벳' => 'Red Velvet',
-			'슬기' => 'Seulgi',
-			'아이린' => 'Irene',
-			'조이' => 'Joy',
-			'웬디' => 'Wendy',
-			'예리' => 'Yeri',
-			'직캠' => 'Fancam',
-			'태연' => 'Taeyeon',
-			'나연' => 'Naeyon',
-			'우기' => 'Yuqi',
-			'수진' => 'Soojin',
-			'소연' => 'Soyeon',
-			'미연' => 'Miyeon',
-			'민니' => 'Minnie',
-			'슈화' => 'Shuhua'
-		];
+		$traducoes = Traducao::traducoes();
 
 		foreach ($traducoes as $original => $traduzido) {
 			$titulo = \str_replace($original, $traduzido, $titulo);
